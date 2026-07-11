ad5f1fe2a6fd23287e51c0ffc5c5246bdb82d34e
okonma <okonmaviolet@gmail.com>
2026-07-12 00:20:30 +0500

    309

--- common.go
+++ common.go
@@ -3,6 +3,7 @@ package main
 import (
 	"math"
 
+	"gonum.org/v1/gonum/floats"
 	"gonum.org/v1/gonum/stat"
 )
 
@@ -19,12 +20,26 @@ func truncatePrec(n float64) float64 {
 // }
 
 func (item *stat_products_item) calcStats() {
+	var ok bool
 	for i := range item.Suppliers_stat {
 		if len(item.Suppliers_stat[i].stocks_nozeroes) > 0 {
-			item.Suppliers_stat[i].Stock_avg = truncatePrec(stat.Mean(item.Suppliers_stat[i].stocks, nil))
-			item.Suppliers_stat[i].Stock_avg_nozeroes = truncatePrec(stat.Mean(item.Suppliers_stat[i].stocks_nozeroes, nil))
-			item.Suppliers_stat[i].Price_avg = truncatePrec(stat.Mean(item.Suppliers_stat[i].prices, nil))
-			item.Suppliers_stat[i].Stock_dispersion = truncatePrec(stat.PopVariance(item.Suppliers_stat[i].stocks, nil))
+			if ok, item.Suppliers_stat[i].Stock_avg = meanSafe(item.Suppliers_stat[i].stocks); ok {
+				item.Suppliers_stat[i].Stock_avg = truncatePrec(item.Suppliers_stat[i].Stock_avg)
+				item.Suppliers_stat[i].Stock_dispersion = truncatePrec(stat.PopVariance(item.Suppliers_stat[i].stocks, nil))
+			}
+			if ok, item.Suppliers_stat[i].Stock_avg_nozeroes = meanSafe(item.Suppliers_stat[i].stocks_nozeroes); ok {
+				item.Suppliers_stat[i].Stock_avg_nozeroes = truncatePrec(item.Suppliers_stat[i].Stock_avg_nozeroes)
+			}
+			if ok, item.Suppliers_stat[i].Price_avg = meanSafe(item.Suppliers_stat[i].prices); ok {
+				item.Suppliers_stat[i].Price_avg = truncatePrec(item.Suppliers_stat[i].Price_avg)
+			}
 		}
 	}
+}
+
+func meanSafe(v []float64) (bool, float64) {
+	if s := floats.Sum(v); s > 0 {
+		return true, s / float64(len(v))
+	}
+	return false, 0
 }
